59c92c3966b9ab7fed1e990d5d785c62dc3a1ccc
UroborusEx <bmv2096@yandex.ru>
2017-12-18 00:08:32 +0300

    Now REST works with UI

---
+++ app/src/main/java/com/meatballs/client/YouTrackPreference.java
@@ -0,0 +1,13 @@
+package com.meatballs.client;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+
+public class YouTrackPreference extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.youtrack_prefrences);
+    }
+}
